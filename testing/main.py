cae2c6d14c7d3aff07873da701e88de2a6548a64
Ivo Pletikosic <37150305+ifpleti@users.noreply.github.com>
2020-06-29 04:18:36 -0400

    crear modulos
    lo mismo pero mas ordenado

---
+++ testing/main.py
@@ -0,0 +1,14 @@
+from modules.airbnb_scraping import scrape
+
+def main():
+    city = "Viña del Mar"
+    checkin = "2020-06-29"
+    checkout = "2020-06-30"
+    adults = 2
+    children = 2
+    babies = 1
+
+    scrape(city, checkin, checkout, adults, children, babies)
+
+if __name__ == "__main__":
+    main()
